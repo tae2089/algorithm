0ce0e17336cc94a8cc5451d4c881de531f11a212
tae2089 <tae2089002@gmail.com>
2021-08-09 21:13:14 +0900

    백준 2606 바이러스 dfs

---
+++ python/backjun/test5.py
@@ -0,0 +1,28 @@
+def average(num1, num2):
+
+    result = (num1+num2)/2
+
+    return result
+
+def mul(num1, num2):
+
+    print(num1*num2)
+
+ 
+
+
+def increase(num):
+
+    result = num + 3
+    print(result+3)
+
+
+def div(num1, num2):
+
+    return (num1 / num2)
+
+
+print(average(7,8))
+print(mul(5,9))
+print(increase(3))
+print(div(6,2))
