ea4e4430d37843ac76a3310cc41bb8000c89bc5e
tae2089 <tae2089002@gmail.com>
2022-06-29 19:13:05 +0900

    2588 백준

--- go/2588.go
+++ go/2588.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"fmt"
-	"strings"
+	"strconv"
 )
 
 func main()  {
@@ -10,17 +10,17 @@ func main()  {
 	fmt.Scanln(&input1)
 	var input2 string
 	fmt.Scanln(&input2)
-	input2s := strings.Split(input2, "")
-	// for i := len(input2s)-1; 0 <= i; i--{
-	// 	input2sIndexValue ,err := strconv.Atoi(string(input2s[i]))
-	// 	if err != nil {
-	// 		panic(err)
-	// 	}
-	// 	fmt.Println(input2CharArrayIntValue*int(input1))
-	// }
-	// input2IntValue, err := strconv.Atoi(input2)
-	// if err != nil{
-	// 	panic(err)
-	// }
-	// fmt.Println(input1*int32(input2IntValue))
+	input2CharArray := []rune(input2)
+	for i := len(input2CharArray)-1; 0 <= i; i--{
+		input2CharArrayIntValue,err := strconv.Atoi(string(input2CharArray[i]))
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println(input2CharArrayIntValue*int(input1))
+	}
+	input2IntValue, err := strconv.Atoi(input2)
+	if err != nil{
+		panic(err)
+	}
+	fmt.Println(input1*int32(input2IntValue))
 }
